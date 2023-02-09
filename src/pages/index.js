8d8a18fff9b98809e13c5123afc00c4bac3b765b
dagfinndybvig <dagfinn.dybvig@gmail.com>
2023-02-09 11:58:29 +0100

    style list

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,28 @@
 import React from 'react';
 
+const listStyles = {
+  listStyleType: "none",
+  padding: 50,
+};
+
 class IndexPage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: null
+      data: []
     };
   }
 
   componentDidMount() {
     // Make the GET request to the API endpoint
-    fetch('https://folk.ntnu.no/dybvig/mypage/respond.php')
-      .then(response => response.json())
-      .then(data => this.setState({ data }));
+    fetch('https://folk.ntnu.no/dybvig/mypage/respond2.php')
+    .then(response => response.json())
+    .then(data => {
+      const arrayData = Array.isArray(data) ? data : Object.values(data);
+      this.setState({ data: arrayData });
+    });
   }
+  
 
   render() {
     const { data } = this.state;
@@ -25,8 +34,12 @@ class IndexPage extends React.Component {
 
     // Render the received data
     return (
-      <div><h1>{data.title}</h1>
-      <h2>Greetings from {data.status}</h2></div>
+      <>
+      <h1>Greetings from</h1>
+      <ul style={listStyles}>
+ {data.map(d => (<li>{d.navn} the {d.type}</li>))} 
+      </ul>
+     </>
     );
   }
 }
